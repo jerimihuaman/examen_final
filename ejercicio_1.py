7418e37f6220bfad066d86780ea36ae377f624e3
andre <cancino_12_21@outlook.es>
2021-02-23 20:35:52 -0500

    version 1

---
+++ ejercicio_1.py
@@ -0,0 +1,18 @@
+class Proceso:
+    def __init__(self):
+        pass
+    def retroceso(self, palabra):
+        metodo_cadena = ""
+        for i in cadena:
+            metodo_cadena += i
+            if i == "#":
+                metodo_cadena = metodo_cadena[:-2]
+        print(metodo_cadena)
+    
+cadena = str(input("Introduzca el texto: "))
+
+michi = Proceso()
+
+michi.retroceso(cadena)
+
+    
